9db06dfc6c238da95cebbee705510dd8ccfc0a57
AimFlying <contact.aimflying@gmail.com>
2023-06-18 12:45:48 +0200

    Fixed interactionCreate event

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,9 +1,23 @@
 const { Events } = require('discord.js');
 
 module.exports = {
-	name: Events.ClientReady,
-	once: true,
-	execute(client) {
-		console.log(`Ready! Logged in as ${client.user.tag}`);
+	name: Events.InteractionCreate,
+	async execute(interaction) {
+		if (!interaction.isChatInputCommand()) return;
+
+		const command = interaction.client.commands.get(interaction.commandName);
+
+		if (!command) {
+			console.error(`No command matching ${interaction.commandName} was found.`);
+			return;
+		}
+
+		try {
+			await command.execute(interaction);
+		}
+		catch (error) {
+			console.error(`Error executing ${interaction.commandName}`);
+			console.error(error);
+		}
 	},
 };
