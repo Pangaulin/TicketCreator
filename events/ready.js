c05a931e73c01269fc554456efaf903a1451168c
AimFlying <contact.aimflying@gmail.com>
2023-06-22 20:09:47 +0200

    Added status on the bot + dnd mode

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,14 @@
-const { Events } = require('discord.js');
+// eslint-disable-next-line no-unused-vars
+const { Events, Client, ActivityType } = require('discord.js');
 
 module.exports = {
 	name: Events.ClientReady,
 	once: true,
+	/**
+	 * @param {Client} client
+	 */
 	execute(client) {
+		client.user.setPresence({ status: 'dnd', activities: [{ name: '/setup', type: ActivityType.Listening }] });
 		console.log(`Ready! Logged in as ${client.user.tag}`);
 	},
 };
